7de047b6740de688917a866e8742cd98bf9915c7
Shay <shalinsingh818@gmail.com>
2022-03-25 10:29:53 -0400

    test

---
+++ portsf/main.c
@@ -0,0 +1,42 @@
+/**
+Write sinewave as text
+*/
+
+
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <math.h>
+
+#ifndef M_PI
+#define M_PI (3.141592654)
+#endif
+
+void scaleProgram(){
+	int note, i; 
+	char key[3]; 
+	
+}
+
+
+void generateSineWave(){	
+	int i,nsamps;
+	double samp;
+	double twopi = 2.0 * M_PI;
+	double angleincr;
+	/* set number of points to create */	
+	nsamps = 50;
+	/* make one complete cycle */
+	angleincr = twopi / nsamps;
+	for(i=0; i < nsamps; i++){
+		samp = sin(angleincr *i);
+		fprintf(stdout,"%.8lf\n",samp);
+	}
+	fprintf(stderr,"done\n");
+}
+
+int main(int argc, char** argv){
+	
+	generateSineWave(); 
+	return 0;
+
+}
