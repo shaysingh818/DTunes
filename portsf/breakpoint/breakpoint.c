c2ac3db5a5739906ff29ef470f662a76647c6456
shaysingh818 <shalinsingh818@gmail.com>
2022-05-16 08:26:42 -0400

    More breakpoints

--- portsf/breakpoint/breakpoint.c
+++ portsf/breakpoint/breakpoint.c
@@ -1,8 +1,32 @@
 #include "breakpoint.h"
 
 
+float larger(float first, float second){
+	if(first < second){
+		return second; 
+	}else{
+		return first; 
+	}
+}
+
+
+breakpoint_t maxpoint(const breakpoint_t* points, long npoints){
+	// create a temp breakpoint
+	breakpoint_t point; 
+	point.time = points[0].time; 
+	point.value = points[0].value; 
+	// check for largest point
+	for(int i = 0; i < npoints; i++){
+		if(point.value < points[i].value){
+			point.value = points[i].value; 
+			point.time = points[i].time; 
+		}
+	}
+} 
+
 
 breakpoint_t* getBreakpoints(FILE *fp, long* psize){
+
 	int got; 
 	long npoints = 0, size = 64; 
 	double lasttime = 0.0; 
@@ -14,7 +38,7 @@ breakpoint_t* getBreakpoints(FILE *fp, long* psize){
 	}
 
 	// allocate breakpoints for size of file
-	points = (breakpoint_t*)malloc(sizeof(breakpoint_t) * size)
+	points = (breakpoint_t*)malloc(sizeof(breakpoint_t) * size);
 	if(points == NULL){
 		return NULL; 
 	}
@@ -26,8 +50,49 @@ breakpoint_t* getBreakpoints(FILE *fp, long* psize){
 			&points[npoints].time, 
 			&points[npoints].value
 		); 
+
+		if(got < 0){
+			continue; 
+		}
+
+		if(got == 0){
+			printf("Line %ld has non numeric data\n", npoints+1); 
+			break; 
+		}
+
+		if(got == 1){
+			printf("Incomplete breakpoint found at point %ld\n", npoints+1); 
+			break; 
+		}
+
+		if(points[npoints].time < lasttime){
+			printf("data error at point %ld: time not increasing\n", npoints+1);
+			break; 
+		}
+
+
+		lasttime = points[npoints].time; 
+		if(++npoints == size){
+			breakpoint_t* tmp; 
+			size += npoints; 
+			tmp = (breakpoint_t*)realloc(points, sizeof(breakpoint_t) * size); 
+			if(tmp == NULL){
+				npoints = 0; 
+				free(points); 
+				points = NULL; 
+				break; 
+			}
+			points = tmp; 
+		}
 		
 	}
+
+
+	if(npoints){
+		*psize = npoints; 
+	}
+
+	return points; 
 
 } 
 
--- portsf/breakpoint/breakpoint.h
+++ portsf/breakpoint/breakpoint.h
@@ -16,7 +16,8 @@ struct TickPoint {
 
 typedef struct TickPoint tickpoint_t; 
 
-
+float larger(float first, float second); 
+breakpoint_t maxpoint(const breakpoint_t* points, long npoints); 
 breakpoint_t* getBreakpoints(FILE *fp, long* psize); 
 
 
