c6cf6051c06ea888016460d8ddb030bd1d25cd79
shaysingh818 <shalinsingh818@gmail.com>
2024-11-03 08:21:47 -0600

    first push on branch

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,7 @@ async function greet() {
     <h1>Welcome to Tauri + Vue</h1>
 
     <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+        <h1>Welcome to DTunes</h1>
     </div>
     <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
 
