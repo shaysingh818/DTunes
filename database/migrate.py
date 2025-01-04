2c803148e694911b0690698c36f734f24dfd00c2
shaysingh818 <shalinsingh818@gmail.com>
2025-01-04 11:42:09 -0600

    send tests to CI CD

--- database/migrate.py
+++ database/migrate.py
@@ -74,6 +74,7 @@ TESTING_DB_PATH = "tests"
 APP_NAME = "dtunes-audio-app"
 
 def main():
+    commit_test_dbs("tests/", SCRIPTS_FOLDER); 
     commit(SCRIPTS_FOLDER, f'build/{APP_NAME}.sqlite3')
 
 
