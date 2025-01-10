cd0ba974fdda28b12492716df38c78cac51a3849
shaysingh818 <shalinsingh818@gmail.com>
2025-01-10 12:34:43 -0600

    gh page branch

--- dtunes-site/vite.config.ts
+++ dtunes-site/vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dtunes/",
+  base: "/dtunes-studio/",
   plugins: [
     vue(),
     vueDevTools(),
